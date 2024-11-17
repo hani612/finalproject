cb20e395c5d99c3f3bde0d9783fec2b87db58fe4
hani612 <hinazahoor80@gmail.com>
2024-11-17 16:51:25 +0000

    Initial commit

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,7 +10,7 @@ class FashionMNIST_CNN(nn.Module):
         super(FashionMNIST_CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(64 * 6 * 6, 128)
+        self.fc1 = nn.Linear(64 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -18,7 +18,7 @@ class FashionMNIST_CNN(nn.Module):
         x = torch.max_pool2d(x, 2)
         x = torch.relu(self.conv2(x))
         x = torch.max_pool2d(x, 2)
-        x = x.view(-1, 64 * 6 * 6)
+        x = x.view(-1, 64 * 5 * 5) 
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
